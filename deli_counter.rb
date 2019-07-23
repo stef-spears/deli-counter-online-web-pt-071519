a5114a4020da1a6bab326f89fd2e3ef4b9165ab4
Stef Spears <spears.sj87@gmail.com>
2019-07-23 17:49:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,14 @@
 katz_deli = []
 
 def line(katz_deli) 
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-  else 
+  if katz_deli.length !== 0
     lineup = "The line is currently: "
     katz_deli.each.with_index(1) do |value, index|
       lineup << "#{index}. #{value} "
       end
+       puts lineup
     end
-  
-  puts lineup
+  else puts "The line is currently empty."
 end
 
 def take_a_number(katz_deli, name)
